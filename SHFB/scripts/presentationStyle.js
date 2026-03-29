d86564659b6164ae1bca4c5b4de8f7cb0a828005
EWSoftware <Eric@EWoodruff.us>
2026-03-29 11:32:55 -0700

    Documentation refresh

--- SHFB/scripts/presentationStyle.js
+++ SHFB/scripts/presentationStyle.js
@@ -299,7 +299,7 @@ function LoadToc()
     const selectedTopicId = $("meta[name='guid']").attr("content");
 
     $.ajax({
-        url: "../toc.json?v=6D6E9103",
+        url: "../toc.json?v=567A7AE5",
         dataType: "json",
         success: function (data)
         {
@@ -497,7 +497,7 @@ function PerformSearch()
         searchResults.innerHTML = "Loading index...";
 
         $.ajax({
-            url: "searchIndex.json?v=6D6E9103",
+            url: "searchIndex.json?v=567A7AE5",
             dataType: "json",
             success: function (data)
             {
@@ -515,7 +515,7 @@ function PerformSearch()
     if(!fileIndex)
     {
         $.ajax({
-            url: "fileIndex.json?v=6D6E9103",
+            url: "fileIndex.json?v=567A7AE5",
             dataType: "json",
             success: function (data)
             {
